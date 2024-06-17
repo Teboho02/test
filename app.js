ed1475350024ed08544a0b6b536088bb4e6b9c6f
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2024-06-17 02:17:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    showInstallPromotion();
+});
+
+const installButton = document.getElementById('install-button');
+installButton.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+    });
+});
