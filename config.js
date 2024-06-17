b4984688bff7fba7f2cd4e6264a4e4f18af11983
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2024-06-17 21:17:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
 
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFBwd8yFSrkKvZikexNWb8VMRozAe1mEk",
+  authDomain: "find-my-patner.firebaseapp.com",
+  projectId: "find-my-patner",
+  storageBucket: "find-my-patner.appspot.com",
+  messagingSenderId: "892330665624",
+  appId: "1:892330665624:web:1d597128bb74982e9a52ed"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
